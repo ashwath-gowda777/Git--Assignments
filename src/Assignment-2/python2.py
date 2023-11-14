d5fae0d6e3ca221bf43d37ae5ec89f8f219f5936
Ashwath-gowda777 <ashwathgowda216@gmail.com>
2023-11-14 18:48:32 +0530

    python file2 is created

---
+++ src/Assignment-2/python2.py
@@ -0,0 +1,11 @@
+x=int(input("Enter the value1 = "))
+y=int(input("Enter the value2 = "))
+i=1
+hcf=1
+while i<=x or i<=y:
+    if x%i==0 and y%i==0:
+        hcf=i
+    i+=1
+print(hcf)
+lcm=hcf*(x//hcf)*(y//hcf)
+print()
