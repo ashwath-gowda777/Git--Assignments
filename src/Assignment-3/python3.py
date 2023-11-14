d6b79dc57156a26825c52458b5f2ed3b1278ce26
Ashwath-gowda777 <ashwathgowda216@gmail.com>
2023-11-15 00:26:08 +0530

    Modifying the code

--- src/Assignment-3/python3.py
+++ src/Assignment-3/python3.py
@@ -6,7 +6,9 @@ for i in range(n):
     a,b =b,a+b
 
 
-print("git")
-print("code")
-
-print("code2")
+n=int(input("Enter the input value: "))
+a=0
+b=1
+for i in range(n):
+    print(a)
+    a,b =b,a+b
